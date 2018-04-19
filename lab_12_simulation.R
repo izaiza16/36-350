dc977c1f8b1512be7df38d03a097f25e5467f5a1
Izabela <Izabela@MacBook-Pro-Izabela.local>
2018-04-18 23:41:34 -0400

    Second function

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -5,3 +5,16 @@ generate_data = function(n,p) {
   v=rnorm(num, mean=0, sd=1)
   list(covariates = m, responses = v)
 }
+
+
+
+model_select = function(covariates, responses, cutoff) {
+  reg = lm(responses ~ covariates)
+  t = t.test(responses, covariates)[3]
+  if (t>cutoff) {
+    
+  } 
+  if (t>=cutoff) {
+    
+  }
+}
