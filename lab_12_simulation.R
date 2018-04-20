2f2ac7ef5a5bc1f8f4a5dc75d4cf878df1f541d4
Izabela <Izabela@MacBook-Pro-Izabela.local>
2018-04-19 20:18:08 -0400

    NEW

--- lab_12_simulation.R
+++
@@ -1,9 +0,0 @@
-
-
-generate_data = function(n,p) {
-  num = n
-  per = p
-  m=matrix(rnorm(num*per, mean=0, sd=1), nrow=num, ncol=per)
-  v=rnorm(num, mean=0, sd=1)
-  list(covariates = m, responses = v)
-}
