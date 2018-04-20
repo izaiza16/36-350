ec118e1876fea71378be79a59e3cf79665f51201
Izabela <Izabela@MacBook-Pro-Izabela.local>
2018-04-19 20:20:31 -0400

    Function 2

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -8,3 +8,19 @@ generate_data = function(n,p) {
   list(covariates = m, responses = v)
 }
 
+
+model_select = function(covariates, responses, cutoff) {
+  num = ncol(covariates)
+  pvalues = summary(lm(responses ~ covariates))$coefficients[,4]
+  if (length(pvalues) > 0) {
+    pvalues = summary(lm(responses ~ covariates))$coefficients[,4][-1]
+    covariates = covariates[, c(is.na(pvalues)==FALSE)]
+    covariates = covariates[, c(cutoff<pvalues)]
+    pvalues = summary(lm(responses ~ covariates))$coefficients[,4][-1]
+    return(pvalues)
+  }
+  if (length(pvalues) == 0) {
+    return(c())
+  }
+}
+
